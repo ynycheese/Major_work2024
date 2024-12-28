9fc34c20e3faddda40ca0bb6e3b94558f119d00f
Yen Trang Tran <yentrang1007@gmail.com>
2024-12-28 23:25:01 +1100

    developed smth

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, redirect, url_for, session, request, make_response, send_from_directory
+import sqlite3
+import os
+
+app = Flask(__name__)
+app.secret_key = os.getenv('SECRET_KEY', 'default_key')
+
+def get_db_connection():
+    connection = sqlite3.connect('business-database.db')
+    connection.row_factory = sqlite3.Row
+    return connection
+
+@app.route('/')
+def homepage():
+    connection = get_db_connection()
--- database.py
+++ database.py
@@ -1,1 +1,66 @@
 import sqlite3
+
+connection = sqlite3.connect('my-database.db')
+cursor = connection.cursor()
+
+
+def get_db_connection():
+    """Connect to the SQLite database and return the connection object."""
+    connection = sqlite3.connect('business-database.db')
+    connection.row_factory = sqlite3.Row 
+    return connection
+
+
+def initialise_database():
+    
+    # create_table_users = '''
+    # CREATE TABLE IF NOT EXISTS users (
+    #     username TEXT NOT NULL UNIQUE,
+    #     password TEXT NOT NULL,
+    #     profile_picture TEXT NOT NULL
+    # );'''
+
+
+    # create_table_reviews = '''
+    # CREATE TABLE IF NOT EXISTS reviews (
+    # id INTEGER PRIMARY KEY AUTOINCREMENT,
+    # game_id INTEGER NOT NULL,
+    # rating INTEGER NOT NULL,
+    # review TEXT,
+    # title TEXT,
+    # date TEXT NOT NULL,
+    # username TEXT NOT NULL,
+    # profile_picture TEXT,
+    # FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE CASCADE
+    # );'''
+
+    # create_table_games = '''
+    # CREATE TABLE IF NOT EXISTS games (
+    # id INTEGER PRIMARY KEY AUTOINCREMENT,
+    # title TEXT NOT NULL,
+    # description TEXT,
+    # tags TEXT,
+    # rating REAL,
+    # image TEXT NOT NULL
+    # );'''
+
+    create_table_products = '''
+    CREATE TABLE IF NOT EXISTS products (
+        id INTERGER PRIMARY KEY AUTOINCREMENT
+        name TEXT NOT NULL
+        image TEXT NOT NULL
+        description TEXT
+        )'''
+
+    cursor.execute("PRAGMA foreign_keys = ON;")
+
+    # cursor.execute(create_table_users)
+    # cursor.execute(create_table_games)
+    # cursor.execute(create_table_reviews)
+
+    connection.commit()
+    connection.close()
+    
+    print('database is created successfully and data is inserted')
+
+initialise_database()
