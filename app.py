fba531b11794731926d55aa23638da4a7195d9a0
Yen Trang Tran <yentrang1007@gmail.com>
2025-01-29 21:56:36 +1100

    Added a database, projected some stuff to front end

--- app.py
+++ app.py
@@ -6,10 +6,16 @@ app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY', 'default_key')
 
 def get_db_connection():
-    connection = sqlite3.connect('business-database.db')
+    connection = sqlite3.connect('website_database.db')
     connection.row_factory = sqlite3.Row
     return connection
 
 @app.route('/')
 def homepage():
     connection = get_db_connection()
+    products = connection.execute('SELECT * FROM product_database ORDER BY RANDOM() LIMIT 12').fetchall()
+    connection.close()
+    return render_template('homepage.html', products=products)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- database.py
+++ database.py
@@ -1,66 +1,25 @@
 import sqlite3
+import pandas as pd
 
-connection = sqlite3.connect('my-database.db')
-cursor = connection.cursor()
+csv_file = './uploads/product-database.csv'
 
+conn = sqlite3.connect('website_database.db')
+cursor = conn.cursor()
 
-def get_db_connection():
-    """Connect to the SQLite database and return the connection object."""
-    connection = sqlite3.connect('business-database.db')
-    connection.row_factory = sqlite3.Row 
-    return connection
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS product_database (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    category TEXT,
+    product TEXT NOT NULL,
+    price REAL NOT NULL,
+    image TEXT
+    )"""
+)
 
+conn.commit()
 
-def initialise_database():
-    
-    # create_table_users = '''
-    # CREATE TABLE IF NOT EXISTS users (
-    #     username TEXT NOT NULL UNIQUE,
-    #     password TEXT NOT NULL,
-    #     profile_picture TEXT NOT NULL
-    # );'''
+df = pd.read_csv(csv_file)
+df.to_sql('product_database', conn, if_exists='append', index=False)
 
-
-    # create_table_reviews = '''
-    # CREATE TABLE IF NOT EXISTS reviews (
-    # id INTEGER PRIMARY KEY AUTOINCREMENT,
-    # game_id INTEGER NOT NULL,
-    # rating INTEGER NOT NULL,
-    # review TEXT,
-    # title TEXT,
-    # date TEXT NOT NULL,
-    # username TEXT NOT NULL,
-    # profile_picture TEXT,
-    # FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE CASCADE
-    # );'''
-
-    # create_table_games = '''
-    # CREATE TABLE IF NOT EXISTS games (
-    # id INTEGER PRIMARY KEY AUTOINCREMENT,
-    # title TEXT NOT NULL,
-    # description TEXT,
-    # tags TEXT,
-    # rating REAL,
-    # image TEXT NOT NULL
-    # );'''
-
-    create_table_products = '''
-    CREATE TABLE IF NOT EXISTS products (
-        id INTERGER PRIMARY KEY AUTOINCREMENT
-        name TEXT NOT NULL
-        image TEXT NOT NULL
-        description TEXT
-        )'''
-
-    cursor.execute("PRAGMA foreign_keys = ON;")
-
-    # cursor.execute(create_table_users)
-    # cursor.execute(create_table_games)
-    # cursor.execute(create_table_reviews)
-
-    connection.commit()
-    connection.close()
-    
-    print('database is created successfully and data is inserted')
-
-initialise_database()
+print('database established')
+conn.close()
